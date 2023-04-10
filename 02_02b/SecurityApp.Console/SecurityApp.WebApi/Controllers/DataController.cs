833fe84805b429ddab96880e293c457e823257f2
Ervis Trupja <ervis.trupja@projekt202.com>
2023-04-10 13:51:38 +0200

    Add content for 02_02b

---
+++ 02_02b/SecurityApp.Console/SecurityApp.WebApi/Controllers/DataController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SecurityApp.WebApi.Data;
+
+namespace SecurityApp.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DataController : ControllerBase
+    {
+        private readonly IDataProtectionProvider _dataProtectionProvider;
+        public DataController(IDataProtectionProvider dataProtectionProvider)
+        {
+            _dataProtectionProvider = dataProtectionProvider;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            string title = "Welcome to this course!";
+
+            var dataProtector = _dataProtectionProvider.CreateProtector("DataController");
+            var protectedTitle = dataProtector.Protect(title);
+            var unProtectedTitle = dataProtector.Unprotect(protectedTitle);
+
+            return Ok(new DataProtection()
+            {
+                Title = title,
+                ProtectedTitle = protectedTitle,
+                UnProtectedTitle = unProtectedTitle
+            });
+        }
+    }
+}
