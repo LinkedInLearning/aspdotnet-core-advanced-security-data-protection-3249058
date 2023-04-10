9137fc3a642a9ca0e6e082ab0d8738143165b63c
Ervis Trupja <ervis.trupja@projekt202.com>
2023-04-10 13:55:26 +0200

    Add content for 04_03b

---
+++ 04_03b/SecurityApp.Console/SecurityApp.WebApi/Algorithms/CustomAuthenticatedEncryptorFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.DataProtection.AuthenticatedEncryption;
+using Microsoft.AspNetCore.DataProtection.KeyManagement;
+using System.Security.Cryptography;
+
+namespace SecurityApp.WebApi.Algorithms
+{
+    public class CustomAuthenticatedEncryptorFactory : IAuthenticatedEncryptorFactory
+    {
+        public IAuthenticatedEncryptor? CreateEncryptorInstance(IKey key)
+        {
+            //Some Code for the Symmetric Algorithm
+
+            //var symmetricAlgorithm = new SymmetricAlgorithm()
+            //{
+            //    Key = new byte[],
+            //    IV = new byte[] {}
+            //}
+
+            return new CustomAuthenticatedEncryptor(null);
+        }
+    }
+}
---
+++ 04_03b/SecurityApp.Console/SecurityApp.WebApi/Program.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.DataProtection.AuthenticatedEncryption;
+using SecurityApp.WebApi.Algorithms;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//Register the custom authenticated encryptor factory
+builder.Services.AddSingleton<IAuthenticatedEncryptorFactory, CustomAuthenticatedEncryptorFactory>();
+
+builder.Services.AddDataProtection();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
