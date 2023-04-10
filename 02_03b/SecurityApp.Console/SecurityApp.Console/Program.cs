7aaa68e86d6b4d25d173500177661cd90f5c5cf7
Ervis Trupja <ervis.trupja@projekt202.com>
2023-04-10 13:52:15 +0200

    Add content for 02_03b

---
+++ 02_03b/SecurityApp.Console/SecurityApp.Console/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.DataProtection;
+using Microsoft.Extensions.DependencyInjection;
+
+// 1. Add Data Protection Services
+var serviceCollection = new ServiceCollection();
+serviceCollection.AddDataProtection();
+var services = serviceCollection.BuildServiceProvider();
+
+// 2. Create an instance of IDataProtector interface
+var dataProtectionProvider = services.GetService<IDataProtectionProvider>();
+var dataProtector = dataProtectionProvider.CreateProtector("FirstExample");
+
+// 3. Protect and Unprotect a payload
+string title = "Welcome to this course!";
+Console.WriteLine($"Original value = {title}");
+
+var protectedTitle = dataProtector.Protect(title);
+Console.WriteLine($"Protected value = {protectedTitle}");
+
+var unprotectedTitle = dataProtector.Unprotect(protectedTitle);
+Console.WriteLine($"UnProtected value = {unprotectedTitle}");
+
+Console.ReadLine();
