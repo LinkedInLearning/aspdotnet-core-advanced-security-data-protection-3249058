628c8f549dd82124fe4324332d638727609368c1
Ervis Trupja <ervis.trupja@projekt202.com>
2023-04-10 13:51:08 +0200

    Add content for 02_01e

---
+++ 02_01e/SecurityApp.Console/SecurityApp.WebApi/Data/DataProtection.cs
@@ -0,0 +1,9 @@
+﻿namespace SecurityApp.WebApi.Data
+{
+    public class DataProtection
+    {
+        public string Title { get; set; }
+        public string ProtectedTitle { get; set; }
+        public string UnProtectedTitle { get; set; }
+    }
+}
