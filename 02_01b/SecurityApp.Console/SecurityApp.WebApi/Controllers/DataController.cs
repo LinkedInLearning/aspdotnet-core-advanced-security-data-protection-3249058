d8deefcd7931812fb08590426f07b5e13462c543
Ervis Trupja <ervis.trupja@projekt202.com>
2023-04-10 13:50:46 +0200

    Add content for 02_01b

---
+++ 02_01b/SecurityApp.Console/SecurityApp.WebApi/Controllers/DataController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SecurityApp.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DataController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
